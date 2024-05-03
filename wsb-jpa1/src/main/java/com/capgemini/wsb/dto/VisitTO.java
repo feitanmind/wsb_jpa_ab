b811a6a75de7c21dc8509fbb2dd9373ac2042b98
Adam Burski <burskiadamwork@gmail.com>
2024-05-03 13:07:21 +0200

    Zmodyfikowanie dto wizyty dodając do niej pola z id doktora i pacjenta

--- wsb-jpa1/src/main/java/com/capgemini/wsb/dto/VisitTO.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/dto/VisitTO.java
@@ -6,12 +6,16 @@ public class VisitTO {
     private Long id;
     private String description;
     private LocalDateTime time;
+    private Long patientId;
+    private Long doctorId;
 
-    public VisitTO(Long id, String description, LocalDateTime time)
+    public VisitTO(Long id, String description, LocalDateTime time, Long patientId, Long doctorId)
     {
         this.id = id;
         this.description = description;
         this.time = time;
+        this.patientId = patientId;
+        this.doctorId = doctorId;
     }
 
     public Long getId() {
@@ -36,5 +40,21 @@ public class VisitTO {
 
     public void setTime(LocalDateTime time) {
         this.time = time;
+    }
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public Long getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(Long doctorId) {
+        this.doctorId = doctorId;
     }
 }
