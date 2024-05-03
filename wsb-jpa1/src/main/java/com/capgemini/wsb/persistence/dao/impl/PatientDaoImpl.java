77a7b4e9724a86ded661e39a320955a9b956d9d9
Adam Burski <burskiadamwork@gmail.com>
2024-05-03 13:36:07 +0200

    Dodanie implementacji interfejsu Patient Dao

--- wsb-jpa1/src/main/java/com/capgemini/wsb/persistence/dao/impl/PatientDaoImpl.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/persistence/dao/impl/PatientDaoImpl.java
@@ -1,4 +1,10 @@
 package com.capgemini.wsb.persistence.dao.impl;
 
-public class PatientDaoImpl {
+import com.capgemini.wsb.persistence.dao.Dao;
+import com.capgemini.wsb.persistence.dao.PatientDao;
+import com.capgemini.wsb.persistence.entity.PatientEntity;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PatientDaoImpl extends AbstractDao<PatientEntity, Long> implements PatientDao {
 }
