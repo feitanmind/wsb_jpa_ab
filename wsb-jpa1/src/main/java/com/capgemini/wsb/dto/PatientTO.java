2706a0384a2ccbdcd9eaac132ca68cd780a358b7
Adam Burski <burskiadamwork@gmail.com>
2024-05-03 12:03:04 +0200

    Zrobienie wstępnego obiektu dto dla pacjenta

---
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/dto/PatientTO.java
@@ -0,0 +1,69 @@
+package com.capgemini.wsb.dto;
+
+import java.time.LocalTime;
+
+public class PatientTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String telephoneNumber;
+    private String email;
+    private String patientNumber;
+    private LocalTime dataOfBirth;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPatientNumber() {
+        return patientNumber;
+    }
+
+    public void setPatientNumber(String patientNumber) {
+        this.patientNumber = patientNumber;
+    }
+
+    public LocalTime getDataOfBirth() {
+        return dataOfBirth;
+    }
+
+    public void setDataOfBirth(LocalTime dataOfBirth) {
+        this.dataOfBirth = dataOfBirth;
+    }
+}
