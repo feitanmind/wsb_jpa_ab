ec5e62a4228165805adec98d1006a4b2db96f653
Adam Burski <burskiadamwork@gmail.com>
2024-05-03 14:36:24 +0200

    Dodanie mappera dla doktora - mapowanie do obiektu dto

--- wsb-jpa1/src/main/java/com/capgemini/wsb/mapper/DoctorMapper.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/mapper/DoctorMapper.java
@@ -1,4 +1,28 @@
 package com.capgemini.wsb.mapper;
 
-public class DoctorMapper {
+import com.capgemini.wsb.dto.DoctorTO;
+import com.capgemini.wsb.dto.VisitTO;
+import com.capgemini.wsb.persistence.entity.DoctorEntity;
+import com.capgemini.wsb.persistence.entity.VisitEntity;
+
+import java.util.*;
+
+import java.util.ArrayList;
+
+public final class DoctorMapper {
+    public static DoctorTO MapToDoctorTO(DoctorEntity doctorEntity, boolean isChild)
+    {
+        if(doctorEntity == null) return null;
+        if(isChild)
+        {
+            return  new DoctorTO(doctorEntity.getId(),doctorEntity.getFirstName(),doctorEntity.getLastName(),doctorEntity.getTelephoneNumber(), doctorEntity.getEmail(), doctorEntity.getDoctorNumber(), doctorEntity.getSpecialization(),AddressMapper.mapToTO(doctorEntity.getAddress()) ,new ArrayList<>())
+        }
+        List<VisitTO> visits = new ArrayList<>();
+        for(VisitEntity visit : doctorEntity.getVisitsEntities())
+        {
+            visits.add(VisitMapper.MapToVisitTO(visit,true));
+        }
+        return  new DoctorTO(doctorEntity.getId(),doctorEntity.getFirstName(),doctorEntity.getLastName(),doctorEntity.getTelephoneNumber(), doctorEntity.getEmail(), doctorEntity.getDoctorNumber(), doctorEntity.getSpecialization(),AddressMapper.mapToTO(doctorEntity.getAddress()) ,visits);
+
+    }
 }
