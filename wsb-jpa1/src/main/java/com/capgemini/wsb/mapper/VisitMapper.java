f24519e42fa0c31cc56e55d0fdba2b89499a0b96
Adam Burski <burskiadamwork@gmail.com>
2024-05-03 14:38:00 +0200

    Poprawienie mappera wizyt - poprawa mapowania do obiektu to

--- wsb-jpa1/src/main/java/com/capgemini/wsb/mapper/VisitMapper.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/mapper/VisitMapper.java
@@ -1,15 +1,25 @@
 package com.capgemini.wsb.mapper;
 
+import com.capgemini.wsb.dto.DoctorTO;
+import com.capgemini.wsb.dto.PatientTO;
 import com.capgemini.wsb.dto.VisitTO;
 import com.capgemini.wsb.persistence.dao.AddressDao;
 import com.capgemini.wsb.persistence.dao.impl.AddressDaoImpl;
+import com.capgemini.wsb.persistence.entity.AddressEntity;
+import com.capgemini.wsb.persistence.entity.DoctorEntity;
+import com.capgemini.wsb.persistence.entity.PatientEntity;
 import com.capgemini.wsb.persistence.entity.VisitEntity;
 
 public final class VisitMapper {
-    public static VisitTO MapToVisitTO(final VisitEntity visitEntity)
+    public static VisitTO MapToVisitTO(final VisitEntity visitEntity, boolean isChild)
     {
         if(visitEntity == null) return null;
-        return new VisitTO(visitEntity.getId(),visitEntity.getDescription(), visitEntity.getTime(), visitEntity.getPatient().getId(),visitEntity.getDoctor().getId());
+        if(isChild)
+        {
+            //Puste obiekty doctor oraz patient bo java nie ma typów nullowalnych a dla childa jest to nieważne bo wywołane jest z pacjenta bądź doktora
+            return new VisitTO(visitEntity.getId(),visitEntity.getDescription(), visitEntity.getTime(),new DoctorTO(),new PatientTO());
+        }
+        return new VisitTO(visitEntity.getId(),visitEntity.getDescription(), visitEntity.getTime(), DoctorMapper.MapToDoctorTO(visitEntity.getDoctor(),true),PatientMapper.MapToPatientTO(visitEntity.getPatient(),true));
     }
     public static VisitEntity MapToVisitEntity(final VisitTO visitTO)
     {
