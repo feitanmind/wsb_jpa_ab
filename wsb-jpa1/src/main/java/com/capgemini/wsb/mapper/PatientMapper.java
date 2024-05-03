49d12680945f74c4e81eb1dba3088cfb496aa2aa
Adam Burski <burskiadamwork@gmail.com>
2024-05-03 14:22:23 +0200

    Dodanie mappera dla pacjenta - mapowanie z Patient Entity na Patient TO

--- wsb-jpa1/src/main/java/com/capgemini/wsb/mapper/PatientMapper.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/mapper/PatientMapper.java
@@ -1,4 +1,27 @@
 package com.capgemini.wsb.mapper;
+import java.util.*;
+import com.capgemini.wsb.dto.AddressTO;
+import com.capgemini.wsb.dto.PatientTO;
+import com.capgemini.wsb.dto.VisitTO;
+import com.capgemini.wsb.persistence.entity.PatientEntity;
+import com.capgemini.wsb.persistence.entity.VisitEntity;
 
-public class PatientMapper {
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public final class PatientMapper {
+    public static PatientTO MapToPatientTO(PatientEntity patientEntity, boolean isChild)
+    {
+        if(patientEntity == null) return null;
+        if(isChild)
+        {
+            return new PatientTO(patientEntity.getId(),patientEntity.getFirstName(),patientEntity.getLastName(),patientEntity.getTelephoneNumber(),patientEntity.getEmail(), patientEntity.getPatientNumber(), patientEntity.getDateOfBirth(),AddressMapper.mapToTO(patientEntity.getAddress()), new ArrayList<>());
+        }
+        List<VisitTO> visits = new ArrayList<>();
+        for(VisitEntity visitEntity : patientEntity.getVisits())
+        {
+            visits.add(VisitMapper.MapToVisitTO(visitEntity,true));
+        }
+        return new PatientTO(patientEntity.getId(),patientEntity.getFirstName(),patientEntity.getLastName(),patientEntity.getTelephoneNumber(),patientEntity.getEmail(), patientEntity.getPatientNumber(), patientEntity.getDateOfBirth(),AddressMapper.mapToTO(patientEntity.getAddress()), visits);
+    }
 }
